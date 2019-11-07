e895ef4d56809a8d436151bff52e043b3a26d436
rau4o <rkoshkinbayev@gmail.com>
2019-11-07 17:39:51 +0600

    Initial Commit

---
+++ DayOneClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DayOneClone
+//
+//  Created by rau4o on 11/7/19.
+//  Copyright © 2019 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
